436964ee1bfadd32ea9ffb0b2915beae2b167124
finspin <finreg@gmail.com>
2015-08-04 08:33:13 +0300

    One ball bouncing with P2 engine

--- js/game.js
+++ js/game.js
@@ -1,56 +1,52 @@
-var game = new Phaser.Game(480, 640, Phaser.AUTO, '', {
+var game = new Phaser.Game(480, 640, Phaser.AUTO, 'Bubble Game', {
     preload: preload,
     create: create,
-    update: update
+    update: update,
+    render: render
 });
 
-var bubble1, bubble2, bubble3, bubbles;
+var bubble1;
 
 
 function preload() {
     game.load.image('bubble1', 'assets/bubble1.png');
-    game.load.image('bubble2', 'assets/bubble2.png');
-    game.load.image('bubble3', 'assets/bubble3.png');
+    //    game.load.image('bubble2', 'assets/bubble2.png');
+    //    game.load.image('bubble3', 'assets/bubble3.png');
+
+
 }
 
 function create() {
-    game.stage.backgroundColor = '#ccc';
-<<<<<<< HEAD
+    game.stage.backgroundColor = "#000";
+
     game.physics.startSystem(Phaser.Physics.P2JS);
-=======
-    game.physics.startSystem(Phaser.Physics.ARCADE);
->>>>>>> gh-pages
 
-    bubble1 = game.add.sprite(50, game.world.height - 50, 'bubble1');
-    bubble2 = game.add.sprite(200, game.world.height - 50, 'bubble2');
-    bubble3 = game.add.sprite(350, game.world.height - 50, 'bubble3');
+    bubble1 = game.add.sprite(game.world.centerX, game.world.centerY, 'bubble1');
 
-    bubbles = game.add.group();
-    bubbles.add(bubble1);
-    bubbles.add(bubble2);
-    bubbles.add(bubble3);
+    game.physics.p2.enable(bubble1);
 
-    bubbles.enableBody = true;
+    bubble1.body.setCircle(50);
 
-    bubbles.setAll('anchor.x', 0.5);
-    bubbles.setAll('anchor.y', 0.5);
+    var bubbleMaterial = game.physics.p2.createMaterial('bubbleMaterial', bubble1.body);
+    var worldMaterial = game.physics.p2.createMaterial('worldMaterial');
 
-    bubbles.forEach(function (bubble) {
-<<<<<<< HEAD
-        game.physics.p2.enable(bubble);
-        //        game.physics.arcade.enable(bubble);
-=======
-        game.physics.arcade.enable(bubble);
->>>>>>> gh-pages
-        bubble.body.collideWorldBounds = true;
-        bubble.body.bounce.set(1);
-    });
+    game.physics.p2.setWorldMaterial(worldMaterial, true, true, true, true);
 
-    bubble1.body.velocity.setTo(200, 200);
-    bubble2.body.velocity.setTo(250, 250);
-    bubble3.body.velocity.setTo(100, 200);
+    var contactMaterial = game.physics.p2.createContactMaterial(bubbleMaterial, worldMaterial);
+
+    contactMaterial.friction = 0;
+    contactMaterial.restitution = 1.0;
+    contactMaterial.stiffness = 0;
+    contactMaterial.relaxation = 0;
+    contactMaterial.frictionStiffness = 0;
+    contactMaterial.frictionRelaxation = 0;
+    contactMaterial.surfaceVelocity = 0;
+
+    bubble1.body.damping = 0;
+    bubble1.body.velocity.x = 200;
+    bubble1.body.velocity.y = 200;
 }
 
-function update() {
-    game.physics.arcade.collide(bubbles, bubbles);
-}
+function update() {}
+
+function render() {}
